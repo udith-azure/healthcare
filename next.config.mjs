504bd7cb810924e713223f226665b659910ea8ae
udith-azure <puudith@gmail.com>
2024-10-14 17:33:59 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,5 @@
-import { withSentryConfig } from "@sentry/nextjs";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withSentryConfig(nextConfig, {
-  // Use the correct organization associated with your token
-  org: "itm-vocational-university",  // Update this line to match your auth token
-  project: "care-pulse",
-  silent: !process.env.CI,
-  widenClientFileUpload: true,
-  hideSourceMaps: true,
-  disableLogger: true,
-  automaticVercelMonitors: true,
-});
+// Export the config without Sentry
+export default nextConfig;
